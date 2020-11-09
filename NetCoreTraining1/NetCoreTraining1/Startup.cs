221d21b47a5592b82463927e3caadd55f4b293ca
Sean <Sean@SEAN-PC>
2020-11-09 13:06:17 +0000

    Created Database BookListRazor

--- NetCoreTraining1/NetCoreTraining1/Startup.cs
+++ NetCoreTraining1/NetCoreTraining1/Startup.cs
@@ -1,13 +1,15 @@
-using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NetCoreTraining1.Model;
 
 namespace NetCoreTraining1
 {
@@ -23,6 +25,7 @@ namespace NetCoreTraining1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
